d8fb3d2956b42500451df97ee406b477f9f1befe
Artem Garanskyy <garanskyy.artem@protonmail.com>
2025-03-29 20:38:22 +0800

    add site to the json config file bash script created

--- bash-scripts/add_site_to_config_file.sh
+++ bash-scripts/add_site_to_config_file.sh
@@ -9,6 +9,10 @@ DB_NAME=$4
 DB_USER=$5
 DB_PASSWORD=$6
 PHP_VERSION=$7
+PUBLIC_FOLDER=$8
+SSL_ENABLED=$9
+SSL_CERT=${10}
+SSL_KEY=${11}
 
 # Check if the config file exists
 if [ ! -f "$CONFIG_FILE" ]; then
@@ -25,7 +29,25 @@ NEW_SITE=$(jq -n \
     --arg db_user "$DB_USER" \
     --arg db_password "$DB_PASSWORD" \
     --arg php_version "$PHP_VERSION" \
-    '{name: $name, root: $root, server_name: $server_name, db_name: $db_name, db_user: $db_user, db_password: $db_password, php_version: $php_version}')
+    --arg public_folder "$PUBLIC_FOLDER" \
+    --arg ssl_cert "$SSL_CERT" \
+    --arg ssl_key "$SSL_KEY" \
+    --argjson ssl_enabled "$SSL_ENABLED" \
+    '{
+        name: $name,
+        root: $root,
+        server_name: $server_name,
+        db_name: $db_name,
+        db_user: $db_user,
+        db_password: $db_password,
+        php_version: $php_version,
+        public_folder: $public_folder,
+        ssl: {
+            enabled: $ssl_enabled,
+            cert: $ssl_cert,
+            key: $ssl_key
+        }
+    }')
 
 # Add the new site entry to the "sites" array in the JSON file
 jq --argjson new_site "$NEW_SITE" '.docker_settings.sites += [$new_site]' "$CONFIG_FILE" > temp.json && mv temp.json "$CONFIG_FILE"
