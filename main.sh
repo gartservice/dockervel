90da640be8d7878124810dce927188da002ec44e
Artem Garanskyy <garanskyy.artem@protonmail.com>
2025-03-30 09:16:00 +0800

    options to generate docker-compose and nginx config files in main menu were added

--- main.sh
+++ main.sh
@@ -8,13 +8,15 @@ source "./bash-scripts/initialize_database.sh"
 source "./bash-scripts/check_required_apps.sh"
 source "./bash-scripts/generate_env.sh"
 source "./bash-scripts/add_site.sh"
+source "./bash-scripts/generate_docker_compose.sh"
+source "./bash-scripts/generate_nginx_confs.sh"
 
 # Ensure fzf is installed
 ensure_fzf_installed
 
 # Function to show the main menu
 show_menu() {
-    options=("Add New Site" "Build Docker Containers" "Init databases" "Run Migrations" "Check Required Applications" "Generate .env File" "Exit")
+    options=("Add New Site" "Build Docker Containers" "Init databases" "Run Migrations" "Check Required Applications" "Generate .env File" "Generate docker-compose file" "Generate nginx config files" "Exit")
     while true; do
         clear
         echo -e "\n\033[1;36m================================\033[0m"
@@ -31,6 +33,8 @@ show_menu() {
             "Run Migrations") run_migrations; read -p "Press Enter to continue..." ;;
             "Check Required Applications") check_all_apps; read -p "Press Enter to continue..." ;;
             "Generate .env File") generate_env_file; read -p "Press Enter to continue..." ;;
+            "Generate docker-compose file") generate_compose_file; read -p "Press Enter to continue..." ;;
+            "Generate nginx config files") generate_nginx_confs; read -p "Press Enter to continue..." ;;
             "Exit") exit 0 ;;
         esac
     done
